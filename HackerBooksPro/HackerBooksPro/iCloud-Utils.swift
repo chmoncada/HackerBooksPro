77702013279f73bf458b8d579e9b194122bf780d
Charles Moncada <charles.moncada@emoshiapps.com>
2016-09-30 09:24:03 -0500

    Listo para entrega
    En este commit:
    Arreglo de convenciones de nombre de funciones y metodos en Swift 3 y limpieza de descripciones
    
    Siguientes pasos
    a) Pasarle el instruments porque la webView y el mapview consume mucha memoria!!
    
    TODO al final
    c) Arreglar el alerviewcontroller de la camara para ipad
    
    Para v 2.0
    a) Anadir borrado de notas
    c) subrayado de notas (se debe migrar a algun PDF framework
    d) full text search

---
+++ HackerBooksPro/HackerBooksPro/iCloud-Utils.swift
@@ -0,0 +1,9 @@
+//
+//  iCloud-Utils.swift
+//  HackerBooksPro
+//
+//  Created by Charles Moncada on 30/09/16.
+//  Copyright © 2016 Charles Moncada Pizarro. All rights reserved.
+//
+
+import Foundation
